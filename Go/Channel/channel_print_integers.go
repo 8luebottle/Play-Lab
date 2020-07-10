8b12dbecbcf4f83674a48aaa376345897b4ad357
8luebottle <itbiz.irs@gmail.com>
2020-07-10 18:03:46 +0900

    07.10.2020 : Go
    print integer by channel

---
+++ Go/Channel/channel_print_integers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 4
+	b := 5
+
+	cIn := make(chan int, 2) // 2 생성
+	cIn <- a
+	cIn <- b
+
+	getChan(cIn)
+}
+
+func getChan(in chan int) {
+	for i := 0; i < 2; i++ {
+		out := <- in
+		printer(in, out)
+	}
+}
+
+func printer(in chan int, out int) {
+	fmt.Printf("\n in : %v  out : %v", in, out)
+	fmt.Printf("\tin's type: %T | out's type : %T\n", in, out)
+}
+// ********** (Printing Results) *************//
+/*
+ in : 0xc0000b8000  out : 4     in's type: chan int | out's type : int
+
+ in : 0xc0000b8000  out : 5     in's type: chan int | out's type : int
+*/
+
