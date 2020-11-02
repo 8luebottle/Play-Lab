0e62e8ca0be3882f59ba02158b1df0ce28a176b3
8luebottle <itbiz.irs@gmail.com>
2020-11-02 11:15:44 +0900

    11.02.2020 : Go
    TestCode
    - Benchmark

--- Go/TestCode/power_test.go
+++ Go/TestCode/power_test.go
@@ -2,6 +2,7 @@ package TestCode
 
 import "testing"
 
+// Analyzing the performance of Go functions with Benchmarks.
 // Benchmark Example
 func BenchmarkPower(b *testing.B) {
     for i:= 0; i< b.N; i++ {
@@ -9,12 +10,12 @@ func BenchmarkPower(b *testing.B) {
     }
 }
 
-// go -test -run=BenchmarkPower -bench=.
+// go -test -run=Power -bench=.
 /*
     goos: darwin
     goarch: amd64
     pkg: github.com/8luebottle/Play-Lab/Go/TestCode
-    BenchmarkPower-12       42216885                27.3 ns/op
+    BenchmarkPower-12       45195823                26.5 ns/op
     PASS
-    ok      github.com/8luebottle/Play-Lab/Go/TestCode      1.280s
-*/
+    ok      github.com/8luebottle/Play-Lab/Go/TestCode      2.545s
+ */
